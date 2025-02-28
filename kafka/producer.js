0d6ba9ff87e51b501ac93e2aeb15690082128b08
Atharv7901 <atharvshivarkar@gmail.com>
2025-03-01 01:17:41 +0530

    modified producer.js

--- kafka/producer.js
+++ kafka/producer.js
@@ -1,4 +1,10 @@
 const {kafka} = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
@@ -7,18 +13,24 @@ async function init() {
   await producer.connect();
   console.log("Producer connected successfully!");
 
-  await producer.send({
-    topic: "rider-updates",
-    messages: [
-      {
-        partition: 0,
-        key: "location-update",
-        value: JSON.stringify({name: "Tony Stark", loc: "SOUTH"}),
-      },
-    ],
-  });
+  rl.setPrompt("> ");
+  rl.prompt();
 
-  await producer.disconnect();
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({name: riderName, loc: location}),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+  });
 }
 
 init();
